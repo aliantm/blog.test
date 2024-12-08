3d92e52771d3bcad77726173c11c725e2522d46f
aliantm <atfree1988@gmail.com>
2024-12-08 15:23:37 -0500

    components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Welcome to front page</h1>
+    <div class = "max-w-4xl mx-auto px-4">
+        {{--<h1>Welcome to front page</h1>--}}
+        <x-alert2 type="info" class="mb-4">
+            <x-slot name="title">
+                Alert title
+            </x-slot>
+            Alert information
+        </x-alert2>
+        <h1>Hello World</h1>
+    </div>
+    
+    
 </body>
 </html>
