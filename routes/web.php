770afc1500ca65d51383954ef0b215bdcdde5ee2
aliantm <atfree1988@gmail.com>
2024-12-17 17:04:21 -0500

    eloquent_1

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use PhpParser\Node\Stmt\If_;
 use Symfony\Component\Routing\Route as RoutingRoute;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 Route::get('/', HomeController::class);
 
@@ -13,5 +14,40 @@ Route::get('/posts', [PostController::class,'index']);
 Route::get('posts/create', [PostController::class,'create']);
 
 Route::get('posts/{post}', [PostController::class,'show']);
+
+Route::get('prueba', function(){
+/*  //CREATE 
+$post = new Post();
+$post->title = "Titulo de prueba 3";
+$post->content = "Contenido de prueba 3";
+$post->categoria = "Categoria de prueba 3";
+
+$post->save();
+
+return $post;*/
+
+/* UPDATE
+$post = Post::find(1);
+$post = Post::where('title','Titulo de prueba 2')
+        ->first();
+$post->content = "Desarrollo Web";
+$post->save();
+return $post;
+
+*/
+
+/* LISTAR POSTS
+$post = Post::orderBy('categoria','desc')
+        ->select('id','title','categoria')
+        ->take(2)
+        ->get();
+return $post;
+*/
+$post = Post::find(1);
+$post->delete();
+return "Post deleted";
+
+
+});
 
 
