e8e302f1160566490a00b454054980c57816ce8a
aliantm <atfree1988@gmail.com>
2025-01-01 21:29:20 -0500

    markdown

--- resources/views/emails/post-created.blade.php
+++ resources/views/emails/post-created.blade.php
@@ -1,7 +1,10 @@
-<x-app-layout>
-    <h1>Mail to be approved</h1>
-    <p>
-        A new post was created and need to be check
-    </p>
-    <a href="{{route('posts.show',$post)}}"> Post to be approved</a>
-</x-app-layout>
+<x-mail::message>
+# Mail to be approved
+
+<x-mail::panel>
+A new post was created and need to be check
+</x-mail::panel>
+<x-mail::button :url="route('posts.show',$post)" color="success">
+Post to be approved
+</x-mail::button>
+</x-mail::message>
